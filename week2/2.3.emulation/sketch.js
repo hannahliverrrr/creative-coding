f6678cc2eeadabbb123150be0d38e5a7cd551c35
Stephenmcc48 <Stephentmcclanahan@gmail.com>
2024-05-30 23:53:08 -0400

    9 squares

--- week2/2.3.emulation/sketch.js
+++ week2/2.3.emulation/sketch.js
@@ -1,7 +1,57 @@
+let square_size = 78;
+let cols = 3;
+let rows = 3;
+let cushion = 10;
+noLoop();
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(275, 275);
+  noLoop();
 }
 
 function draw() {
-  background(255);
+  background(235);
+
+  translate(44,60)
+  noLoop();
+
+  rectMode(CENTER);
+  angleMode(DEGREES);
+
+for (let x = 0; x < (square_size * cols) ; x += square_size){
+  for (let y = 0; y < (square_size * rows); y += square_size){
+
+    push(); 
+    noLoop();
+
+    translate(x,y);
+
+    fill(224, 102, 91, 220);
+
+    strokeWeight(0);
+
+    let rotation_amount =random(54.2, 24);
+
+
+    let rotate_by = map (x, 0,rotation_amount, 0, rows * square_size);
+    rotate(rotate_by);
+
+    let x_offset = random(24,0) //(-111, 111)
+    let y_offset = random(12,30) //(20,-20)
+
+    let x_shift = map(x,0,rows * rows,0, y_offset);
+    let y_shift = map(y,0,cols * square_size,0, x_offset);
+
+
+
+
+    rect(x_offset,5,square_size - cushion,square_size - cushion)
+
+    console.log('hey');
+
+    pop();
+
+    }
+  }
+
 }
