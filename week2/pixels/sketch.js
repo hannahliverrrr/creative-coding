83f5864104af95cd470e9fb45d5c40ecaae9e26a
Stephenmcc48 <Stephentmcclanahan@gmail.com>
2024-06-08 17:53:06 -0400

    Self Portrait Fix
    The reason for this sites existence is to show the data of how a funky goblin lives. In order to do this, I went on a magical quest out into the world, logging the daily activities that I do that make me feel the most funky goblinist. When planning out this website, I didn't have a plan. I just let things build on themselves naturally. I had a few ideas from the start like the down-here being rotated with different color letters being shown when highlighted over. After that I just started playing around with the hover feature a lot and figured out how to have it make text move. I will say, this self portrait helped me in understanding how to use the scroll feature and positioning features a bit better. I also learned the importance of classes in HTML/CSS

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,13 +1,76 @@
+
+let img;
+let sampleX = 12;
+let sampleY = 14;
+
+
+
 function preload(){
-	img = loadImage();
+	img = loadImage('https://stephenmcc48.github.io/creative-coding/week2/pixels/GreenLantern.jpg');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas (900, 980);
+  img.loadPixels();
   
 }
 
 function draw() {
-  background(220);
-image(img,10,10,100,100);
+  background('forestgreen');
+for (let lineX = 0; lineX < 1; lineX += 1) {
+    for (let lineY = 0; lineY < 2; lineY += 1) {
+ 
+  for (let x = 0; x < 1; x += 1) {
+    for (let y = 0; y < 1; y += 1) {
+
+      
+   
+
+      push();
+
+      
+    
+    translate(x * 100, y * 100);
+
+    
+    stroke(122,34,33, 200);
+    strokeWeight(5)
+   
+    
+
+    stroke(12,23,222);
+    
+
+
+
+    
+
+
+      pop();
+
+strokeWeight( random(3, 2)  );    
+stroke(random(0, 255),120,255);
+    frameRate(7)
+ 
+
+translate(100, 100);
+//image(img, -101, -120);
+
+
+for (let x = -0; x <= img.width; x += sampleX){
+  for (let y = 0; y <= img.height; y += sampleY){
+    var index = (x + y * img.width) * 4;
+    var r = img.pixels[index + 0];
+    var g = img.pixels[index + 1];
+    var b = img.pixels[index + 2];
+
+    fill(r,g,b);
+    rect(x,y,sampleX,sampleY);
+
+          }
+        }
+      }
+    }
+  }
+}
 }
