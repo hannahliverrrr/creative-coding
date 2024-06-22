5c5f1d22c3fa7a2460ff18f82156a19ba6b8f804
Stephenmcc48 <Stephentmcclanahan@gmail.com>
2024-06-22 18:12:30 -0400

    Markov

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,14 +1,31 @@
 
-
-let rm = RiTa.markov(2);
-
-
-
-function preload() {
-  result = loadStrings('');
-}
-
+// create markov model
+let rm = RiTa.markov(5);
+// find data
+let data = document.getElementById("source").innerText;
+// training the model
+rm.addText(data);
+//function preload() {
+ //result = loadStrings('');
+//}
 function setup(){
-
-	
+// generate # of sentences
+let lines = rm.generate(40);
+// finding the corresponding letter DIV
+let letter = select("#letter");
+// add greeting
+letter.child( createP(" Daily Logs From A 19th Century Time Traveler,"));
+for (let l = 2; l < lines.length; l++){
+letter.child(
+createP( lines[l] )
+);
+}
+// add a complimentary closing
+letter.child( createP("It's up to YOU to decide what you do with is information, <br> -The 19th Century Time Traveler"));
+}
+function mousePressed(){
+letter.remove();
+letter = createDiv();
+letter.id("letter");
+setup();
 }
