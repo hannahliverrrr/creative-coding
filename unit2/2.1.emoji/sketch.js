a300139e81a928539008c6b951c5fde51d3a7272
hannahliverrrr <hlivermon@icloud.com>
2024-09-23 23:09:02 -0400

    er
    erve

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -8,30 +8,5 @@ function setup() {
 
 function draw() {
   background(220);
-CSSNumericValue(200,200,100)
-
-  rectMode(CENTER);
-  <script>
-  const canvas = document.getElementById('smileyCanvas');
-  const ctx = canvas.getContext('2d');
-
-  // Draw the face
-  ctx.beginPath();
-  ctx.arc(100, 100, 80, 0, Math.PI * 2, true); // Outer circle
-  ctx.fillStyle = 'yellow';
-  ctx.fill();
-  ctx.stroke();
-
-  // Draw the eyes
-  ctx.beginPath();
-  ctx.arc(70, 70, 10, 0, Math.PI * 2, true);  // Left eye
-  ctx.arc(130, 70, 10, 0, Math.PI * 2, true); // Right eye
-  ctx.fillStyle = 'black';
-  ctx.fill();
-
-  // Draw the mouth
-  ctx.beginPath();
-  ctx.arc(100, 100, 50, 0, Math.PI, false);  // Mouth (clockwise)
-  ctx.stroke();
-</script>
+  circle(200,200,100)
 }
