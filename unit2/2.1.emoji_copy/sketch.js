d17bc054364ab0de1b5eca32a96165742d7e68ed
hannahliverrrr <hlivermon@icloud.com>
2024-09-24 12:35:42 -0400

    drb
    df

--- unit2/2.1.emoji_copy/sketch.js
+++ unit2/2.1.emoji_copy/sketch.js
@@ -8,8 +8,8 @@ function setup() {
 
 function draw() {
   background(220);
-  //i want to draw a vampire face
-  //the face - yellow circle
+  //i want to draw a seal
+  //the face 
   fill(100,100,100)
   stroke(150,150,150)
   strokeWeight(10)
@@ -27,12 +27,11 @@ function draw() {
   ellipse(250,160,35,40,40)
 
   //the mouth 
-  // Bottom-right.
   arc(200, 210, 75, 60, 700, QUARTER_PI);
 
   stroke('black');
   strokeWeight(5);
-
+//the whiskers
   line(100, 190, 150, 200);
   line(100, 220, 150, 220)
   line(300, 190, 250, 200)
