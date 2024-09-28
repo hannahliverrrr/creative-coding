435b172025e5ec139757697fe47c58749d5df595
hannahliverrrr <hlivermon@icloud.com>
2024-09-27 22:09:04 -0400

    ertb
    erb

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -7,10 +7,19 @@ function setup() {
 }
 function draw() {
   background(220);
-
+  translate(150,100)
+//create 8 by 8 grid loop
   for(let x=1; x<9; x+=1){
     for (let y=1; y<9; y +=1){
-    circle(x*60,y*60,50)
+  //set parameters
+    push()
+    translate(x*20,y*20)
+    strokeWeight(random(1,20))
+    stroke(random(0,255),random(0,255),random(0,255))
+    fill(random(0,255),random(0,255),random(0,255))
+    //draw the circle
+    rect(x*90,y*90,random(10-150))
+    pop()
   }
   }
 
