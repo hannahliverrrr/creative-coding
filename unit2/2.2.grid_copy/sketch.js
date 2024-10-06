023be2271b0aab66a29146a377b684134e47965b
hannahliverrrr <hlivermon@icloud.com>
2024-10-06 12:15:24 -0400

    erthb
    rtn

--- unit2/2.2.grid_copy/sketch.js
+++ unit2/2.2.grid_copy/sketch.js
@@ -11,15 +11,16 @@ function draw() {
 //create 8 by 8 grid loop
   for(let x=1; x<9; x+=1){
     for (let y=1; y<9; y +=1){
-  //set parameters
+  //set parameters for rectangles
   push()
       translate(x*20,y*20)
       strokeWeight(random(7,10))
       stroke(random(100,120),random(10,15),random(22,25))
       fill(random(100,155),random(0,55),random(0,55),random(50,100))
+      //draw the rectangles
       rect(x*90,y*90,random(75,90))
       pop()
-
+//set parameters for circles
     push()
     translate(x*20,y*20)
     strokeWeight(random(7,10))
--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -1,13 +1,30 @@
 function setup() {
-  // create the canvas
-  createCanvas(600, 800);
-
-  // disable animation
+  createCanvas(400, 400);
+  background(155,110,0);
   noLoop();
+  noStroke();
 }
-
 function draw() {
-
-  background(255);
-
+  let border = 15; // Border size
+  let spacing = 9; // space between squares
+  let cols = 14; // how many columns
+  let rows = 14; // how many rows
+  let w = (width - 4 * border - (cols - 5) * spacing) / cols; //dimensions & positioning of col
+  let h = (height - 1.5 * border - (rows + 1.5) * spacing) / rows; //dimensions & positioning of row
+  
+  for (let row = 0; row < rows; row++) { // help identify row and color 
+    let c = lerpColor(color(212, 225, 222,215), color(10, 40, 50), row / (rows - 1)); // gradient
+    // used p5.js reference page for lerpColor
+    for (let col = 0; col < cols; col++) { // help identify horizontal position of square
+      let x = border + col * (w + spacing);
+      let y = border + row * (h + spacing);
+    
+      let yOffset = random(-h / 4, h / 4); // offset position of squares
+    
+      y += yOffset;
+      
+      fill(c); // set fill color for the current row
+      rect(x, y, w, h);
+    }
+  }
 }
