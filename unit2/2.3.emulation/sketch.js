9e9bbbe96a1b2499c2fcdc2bf8f3067452250a19
hannahliverrrr <hlivermon@icloud.com>
2024-10-07 00:10:01 -0400

    krb
    fb

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -5,26 +5,26 @@ function setup() {
   noStroke();
 }
 function draw() {
-  let border = 15; // Border size
-  let spacing = 9; // space between squares
+  let border = 15; // border size
+  let spacing = 7; // space between squares
   let cols = 14; // how many columns
   let rows = 14; // how many rows
-  let w = (width - 4 * border - (cols - 5) * spacing) / cols; //dimensions & positioning of col
-  let h = (height - 1.5 * border - (rows + 1.5) * spacing) / rows; //dimensions & positioning of row
+  let w = (width - 2 * border - (cols - 1) * spacing) / cols; // dimensions & positioning of col
+  let h = (height - 2 * border - (rows - 5) * 1.5 * spacing) / rows; // dimensions & positioning of row
   
   for (let row = 0; row < rows; row++) { // help identify row and color 
-    let c = lerpColor(color(212, 225, 222,215), color(10, 40, 50), row / (rows - 1)); // gradient
-    // used p5.js reference page for lerpColor
+    let c = lerpColor(color(200, 220, 255, 190), color(24, 25, 32), row / (rows - 1)); // gradient
     for (let col = 0; col < cols; col++) { // help identify horizontal position of square
       let x = border + col * (w + spacing);
       let y = border + row * (h + spacing);
-    
-      let yOffset = random(-h / 4, h / 4); // offset position of squares
-    
-      y += yOffset;
       
+      let yOffset = random(-h / 8, h / 8); // offset position of squares horizontally
+      
+      y += yOffset;
+
       fill(c); // set fill color for the current row
       rect(x, y, w, h);
+
     } 
   }
 }
