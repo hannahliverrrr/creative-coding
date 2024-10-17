7ab109676c6add72816b4642bbf9b364ba64ca40
hannahliverrrr <hlivermon@icloud.com>
2024-10-16 22:33:31 -0400

    iuj
    o

--- unit2/landscape/sketch.js
+++ unit2/landscape/sketch.js
@@ -1,18 +1,62 @@
-function preload(){
-  // load the image from a file
-	img = loadImage();
-}
+let rocks = [];
+let buildings = [];
 
 function setup() {
-
-  // create the canvas
-  createCanvas(800, 800);
-  
+  createCanvas(800, 400);
+  generateRocks(20); // Generate 20 rocks
+  generateBuildings(10); // Generate 10 buildings
 }
 
 function draw() {
-  background(220);
+  background(135, 206, 235); // Blue sky
+  
+  drawMountains();
+  drawRocks();
+  drawBuildings();
+}
 
-  // render the image
-  image(img,10,10,100,100);
+function drawMountains() {
+  fill(34, 139, 34); // Green color for mountains
+  beginShape();
+  let xoff = 0;
+  for (let x = 0; x <= width; x++) {
+    let y = map(noise(xoff), 0, 1, height - 150, height - 50);
+    vertex(x, y);
+    xoff += 0.02; // Adjust this value to change the smoothness of the mountains
+  }
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+}
+
+function generateRocks(numRocks) {
+  for (let i = 0; i < numRocks; i++) {
+    let x = random(width);
+    let y = random(height - 150, height - 50); // Random y position within the mountain area
+    let size = random(10, 30); // Random size for the rocks
+    rocks.push({x: x, y: y, size: size});
+  }
+}
+
+function drawRocks() {
+  fill(139, 69, 19); // Brown color for rocks
+  for (let rock of rocks) {
+    ellipse(rock.x, rock.y, rock.size, rock.size);
+  }
+}
+
+function generateBuildings(numBuildings) {
+  for (let i = 0; i < numBuildings; i++) {
+    let x = random(width);
+    let widthBuilding = random(20, 50);
+    let heightBuilding = random(50, 150); // Random height for the buildings
+    buildings.push({x: x, width: widthBuilding, height: heightBuilding});
+  }
+}
+
+function drawBuildings() {
+  fill(169, 169, 169); // Grey color for buildings
+  for (let building of buildings) {
+    rect(building.x, height - building.height, building.width, building.height);
+  }
 }
