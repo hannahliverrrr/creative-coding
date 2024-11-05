ce913f533fedb9fabfa41ee8048e8816c364b584
hannahliverrrr <hlivermon@icloud.com>
2024-11-05 15:10:36 -0500

    erg
    ergf

--- unit3/3.x.screen-saver/sketch.js
+++ unit3/3.x.screen-saver/sketch.js
@@ -6,7 +6,11 @@ function setup() {
 
 function draw() {
   background(220);
-  fill(random(255), random(255), random(255)); // Set the fill color to white
-  rectMode(CENTER); // Set the rectangle mode to center
-  rect(width / 2, height / 2, 100, 100); // Draw the square in the center
+  for (let i = 0; i < 100; i++) { // Draw 10 squares
+    fill(random(255), random(255), random(255)); // Set the fill color to a random color
+    let x = random(width); // Random x position
+    let y = random(height); // Random y position
+    rectMode(CENTER); // Set the rectangle mode to center
+    rect(x, y, random(50,100), random(50,100)); // Draw the square at the random position
+  }
 }
