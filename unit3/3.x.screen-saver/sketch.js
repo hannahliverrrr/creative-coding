f992b8c70387278a28fdb91cb0db100f17b0c718
hannahliverrrr <hlivermon@icloud.com>
2024-11-05 15:04:24 -0500

    er

--- unit3/3.x.screen-saver/sketch.js
+++ unit3/3.x.screen-saver/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
+  noStroke(); // Disable the stroke
+  noLoop();
 }
 
 function draw() {
-  background(0);
+  background(220);
+  fill(random(255), random(255), random(255)); // Set the fill color to white
+  rectMode(CENTER); // Set the rectangle mode to center
+  rect(width / 2, height / 2, 100, 100); // Draw the square in the center
 }
