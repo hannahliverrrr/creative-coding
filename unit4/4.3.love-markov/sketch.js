95a1b93e545c928c3385ce9ff9580c6dd0596a0c
hannahliverrrr <hlivermon@icloud.com>
2024-11-21 11:38:04 -0500

    erfv
    v f

--- unit4/4.3.love-markov/sketch.js
+++ unit4/4.3.love-markov/sketch.js
@@ -1,14 +1,30 @@
 
-
 let rm = RiTa.markov(2);
 
-
+let data = document.getElementById("source").innerText;
+rm.addText(data);
 
 function preload() {
   result = loadStrings('');
 }
 
-function setup(){
+function setup() {
+  noCanvas();
+  let lines = rm.generate(6);
+  let letter = select("#letter");
+  letter.child(createP('My Dearest Harrington,'));
+  
+  for (let l = 0; l < lines.length; l++) {
+    let paragraph = createP(lines[l]);
+    letter.child(paragraph);
+  }
+  letter.child(createP('Sincerely, Markov'));
+}
 
-	
+function mousePressed() {
+  let letter = select("#letter");
+  letter.remove();
+  letter = createDiv();
+  letter.id("letter");
+  setup();
 }
