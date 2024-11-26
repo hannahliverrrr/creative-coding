7fe7cfce68ffebabc79857e9c6cecb217e44d961
hannahliverrrr <hlivermon@icloud.com>
2024-11-26 13:03:17 -0500

    rgv
    vr

--- unit4/book/sketch.js
+++ unit4/book/sketch.js
@@ -1,12 +1,48 @@
+let word = "turkey"
 
-let myBook = "";
+let wc = 0;
 
 function setup() {
-noCanvas();
 
+  // We won't need any canvas
+  noCanvas();
 
-
-Bindery.makeBook({
-    content: '#content',
-  });
+ 
+  let content = select("body");
+  content.attribute("id","content");
+  
+  content.child(createElement("h1","Thanksgiving"));
+  
+  
+  //Create the sentence 
+  while( wc < 50000){
+    //repeated header to represent chapters
+    content.child(createElement("h2",String(word+" ").repeat(random(2,6)).toUpperCase()));
+    
+    //sentence parameters, adjusting paragrpah lenght, sentence length, 
+    for (let p = 0; p < random(5,20); p++){
+      let paragraph = '';
+      for (let s = 0; s < random(3,15); s++){
+        let internalPunct = [",",",",",",";"," --"];
+        let endPunct = [".",".","?","!"];
+        let sentence = word.charAt(0).toUpperCase() + word.slice(1);
+        let sentenceLength = random(4,10);
+        wc += sentenceLength + 1;
+        for (let w = 0; w < sentenceLength; w++){
+          sentence += " " + word;
+          if (random() < 0.2){
+            sentence += random(internalPunct);
+          }
+        }
+        sentence += random(endPunct) + " ";
+        paragraph += sentence;
+      }
+      content.child(createElement("p",paragraph));
+    }
+    
+  
+    
+  }
+  
+ window.PagedPolyfill.preview();
 }
