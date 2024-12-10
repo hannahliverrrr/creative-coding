7bf191206391ef3c51da6f963ed47e5caf0bc3d3
hannahliverrrr <hlivermon@icloud.com>
2024-12-10 14:28:05 -0500

    oij
    ok

--- unit2/2.1.emoji_copy/sketch.js
+++ unit2/2.1.emoji_copy/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   // create a canvas
-  createCanvas(400, 400);
+  createCanvas(800, 800); // Adjusted canvas size to fit three seals
 
   // disable animation
   noLoop();
@@ -8,36 +8,46 @@ function setup() {
 
 function draw() {
   background(220);
-  //i want to draw a seal
-  //the face 
-  fill(100,100,100)
-  stroke(150,150,150)
-  strokeWeight(10)
-  circle(200,200,300)
-
-  //draw the eyes
-  fill("black")
-  stroke("#000000")
-  strokeWeight(0.7)
-
-  //left eye 
-  ellipse(155,160,35,40,40)
-
-  //right eye 
-  ellipse(250,160,35,40,40)
-
-  //the mouth 
-  arc(200, 210, 75, 60, 700, QUARTER_PI);
-
-  stroke('black');
-  strokeWeight(5);
-//the whiskers
-  line(100, 190, 150, 200);
-  line(100, 220, 150, 220)
-  line(300, 190, 250, 200)
-  line(300, 220, 250, 220)
-
- 
-
   
+  // Draw three seals
+  drawSeal(200, 650);
+  drawSeal(400, 400);
+  drawSeal(600, 150);
+}
+
+function drawSeal(x, y) {
+  // the face 
+  fill(100, 100, 100);
+  stroke(150, 150, 150);
+  strokeWeight(10);
+  circle(x, y, 300);
+
+  // draw the eyes
+  fill("black");
+  stroke("#000000");
+  strokeWeight(0.7);
+
+  // left eye 
+  ellipse(x - 45, y - 40, 35, 40);
+
+  // right eye 
+  ellipse(x + 45, y - 40, 35, 40);
+
+  // the nose
+  fill(0);
+  stroke(0);
+  strokeWeight(1);
+  ellipse(x, y, 20, 15);
+
+  // the mouth 
+  noFill();
+  stroke(0);
+  strokeWeight(5);
+  arc(x, y + 50, 75, 60, 0, PI);
+
+  // the whiskers
+  line(x - 100, y - 10, x - 50, y);
+  line(x - 100, y + 20, x - 50, y + 20);
+  line(x + 100, y - 10, x + 50, y);
+  line(x + 100, y + 20, x + 50, y + 20);
 }
