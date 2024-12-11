78839c37c8fb01968a61fcacbdcbb2e4f293d313
hannahliverrrr <hlivermon@icloud.com>
2024-12-10 22:29:09 -0500

    lkm
    klm

--- unit5/final/sketch.js
+++ unit5/final/sketch.js
@@ -10,19 +10,21 @@ function setup() {
   //generate random colors for the gradient below
   color1 = color(random(255), random(255), random(255)); 
   color2 = color(random(255), random(255), random(255)); 
+  //used for grid rotation by cursor position
   angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
 
-  
+  //maps out mouse position
   let angleX = map(mouseY, 0, height, -180, 180);
   let angleY = map(mouseX, 0, width, -180, 180);
 
+  //rotate the grid with the mouse movement
   rotateX(angleX);
-  rotateY(angleY * 0.3);
-  rotateZ(angleY * 0.1);
+  rotateY(angleY * .2); //controls rotation sensitivity so it's not so speedy
+  rotateZ(angleY * .2); //same thing as above - don't want it rotating so fast
 
   let spacing = 50;
   let size = 40;
