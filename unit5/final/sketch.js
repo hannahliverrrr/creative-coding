5b2ae8adf3f761df941bf09f24c4266b35c98fab
hannahliverrrr <hlivermon@icloud.com>
2024-12-10 22:23:13 -0500

    lkm
    ;lm

--- unit5/final/sketch.js
+++ unit5/final/sketch.js
@@ -1,37 +1,44 @@
-let bubbles = [];
+
+
+//define color variables
+let color1;
+let color2;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600, WEBGL); //call in 3D mode
+
+  //generate random colors for the gradient below
+  color1 = color(random(255), random(255), random(255)); 
+  color2 = color(random(255), random(255), random(255)); 
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
-  for (let bubble of bubbles) {
-    bubble.move();
-    bubble.display();
-  }
-}
 
-function mousePressed() {
-  let b = new Bubble(mouseX, mouseY);
-  bubbles.push(b);
-}
+  
+  let angleX = map(mouseY, 0, height, -180, 180);
+  let angleY = map(mouseX, 0, width, -180, 180);
 
-class Bubble {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.diameter = random(20, 50);
-    this.speed = random(1, 3);
-  }
+  rotateX(angleX);
+  rotateY(angleY * 0.3);
+  rotateZ(angleY * 0.1);
 
-  move() {
-    this.y -= this.speed;
-  }
+  let spacing = 50;
+  let size = 40;
 
-  display() {
-    noStroke();
-    fill(255, 100);
-    ellipse(this.x, this.y, this.diameter, this.diameter);
+  for (let x = -150; x <= 150; x += spacing) {
+    for (let y = -150; y <= 150; y += spacing) {
+      for (let z = -150; z <= 150; z += spacing) {
+        let t = map(x + y + z, -450, 450, 0, 1);
+        let c = lerpColor(color1, color2, t);
+        stroke(c);
+        noFill();
+        push();
+        translate(x, y, z);
+        box(size);
+        pop();
+      }
+    }
   }
 }
